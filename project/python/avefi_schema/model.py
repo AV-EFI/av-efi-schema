c0ee1733c6853ceacac8af71681b899f818302df
Elias Oltmanns <elias.oltmanns@gwdg.de>
2025-09-16 20:03:39 +0200

    Update derivatives

--- project/python/avefi_schema/model.py
+++ project/python/avefi_schema/model.py
@@ -1,5 +1,5 @@
 # Auto generated from model.yaml by pythongen.py version: 0.0.1
-# Generation date: 2025-09-15T12:58:00
+# Generation date: 2025-09-16T19:53:00
 # Schema: model
 #
 # id: https://www.av-efi.net/av-efi-schema/model
